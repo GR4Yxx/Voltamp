b9a8471f079bcfa6b3d56fa1116330670318f38c
GR4Yxx <layfonalseif10@gmail.com>
2024-07-09 01:03:11 +0530

    removed unec files

--- test.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-# Example datetime object
-sent_timestamp = datetime.now()
-
-# Format the datetime object
-sent_formatted_timestamp = sent_timestamp.strftime("%dth %B, %Y at %H:%M")
-
-print(formatted_timestamp)
